ea305444775f62d6d3da444151421208a18ea52c
pmtavares <pedrotavares41@gmail.com>
2020-02-27 22:49:58 +0000

    re-estructured the folders and add hot module

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+class App extends Component {
+  
+  render(){
+    return (
+      <div className="App">
+        <h1>
+          Tab here
+        </h1>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
